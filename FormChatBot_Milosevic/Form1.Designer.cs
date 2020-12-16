d546998672c527cb5eaa2bc08420fe6cf5566a84
Stefan Milosevic <smilosevic86@outlook.it>
2020-12-16 15:09:00 +0100

    Aggiungere i file di progetto.

---
+++ FormChatBot_Milosevic/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace FormChatBot_Milosevic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Serverip = new System.Windows.Forms.Label();
+            this.Serverport = new System.Windows.Forms.Label();
+            this.txt_severip = new System.Windows.Forms.TextBox();
+            this.txt_serverport = new System.Windows.Forms.TextBox();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.Messagio = new System.Windows.Forms.Label();
+            this.txt_mesaggio = new System.Windows.Forms.TextBox();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.lstbox_messagio_server = new System.Windows.Forms.ListBox();
+            this._hr_ = new System.Windows.Forms.Label();
+            this.@__hr = new System.Windows.Forms.Label();
+            this.errore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Serverip
+            // 
+            this.Serverip.AutoSize = true;
+            this.Serverip.Location = new System.Drawing.Point(90, 15);
+            this.Serverip.Name = "Serverip";
+            this.Serverip.Size = new System.Drawing.Size(54, 13);
+            this.Serverip.TabIndex = 0;
+            this.Serverip.Text = "Server IP:";
+            // 
+            // Serverport
+            // 
+            this.Serverport.AutoSize = true;
+            this.Serverport.Location = new System.Drawing.Point(90, 45);
+            this.Serverport.Name = "Serverport";
+            this.Serverport.Size = new System.Drawing.Size(63, 13);
+            this.Serverport.TabIndex = 1;
+            this.Serverport.Text = "Server Port:";
+            // 
+            // txt_severip
+            // 
+            this.txt_severip.Location = new System.Drawing.Point(172, 7);
+            this.txt_severip.Name = "txt_severip";
+            this.txt_severip.Size = new System.Drawing.Size(100, 20);
+            this.txt_severip.TabIndex = 2;
+            this.txt_severip.Text = "127.0.0.1";
+            // 
+            // txt_serverport
+            // 
+            this.txt_serverport.Location = new System.Drawing.Point(172, 45);
+            this.txt_serverport.Name = "txt_serverport";
+            this.txt_serverport.Size = new System.Drawing.Size(100, 20);
+            this.txt_serverport.TabIndex = 3;
+            this.txt_serverport.Text = "23000";
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(322, 24);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 23);
+            this.btn_connect.TabIndex = 4;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // Messagio
+            // 
+            this.Messagio.AutoSize = true;
+            this.Messagio.Location = new System.Drawing.Point(90, 121);
+            this.Messagio.Name = "Messagio";
+            this.Messagio.Size = new System.Drawing.Size(52, 13);
+            this.Messagio.TabIndex = 5;
+            this.Messagio.Text = "Messagio";
+            // 
+            // txt_mesaggio
+            // 
+            this.txt_mesaggio.Location = new System.Drawing.Point(172, 121);
+            this.txt_mesaggio.Name = "txt_mesaggio";
+            this.txt_mesaggio.Size = new System.Drawing.Size(100, 20);
+            this.txt_mesaggio.TabIndex = 6;
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(322, 117);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(75, 23);
+            this.btn_Send.TabIndex = 7;
+            this.btn_Send.Text = "Invia";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // lstbox_messagio_server
+            // 
+            this.lstbox_messagio_server.FormattingEnabled = true;
+            this.lstbox_messagio_server.Location = new System.Drawing.Point(93, 146);
+            this.lstbox_messagio_server.Name = "lstbox_messagio_server";
+            this.lstbox_messagio_server.Size = new System.Drawing.Size(304, 199);
+            this.lstbox_messagio_server.TabIndex = 8;
+            // 
+            // _hr_
+            // 
+            this._hr_.AutoSize = true;
+            this._hr_.Location = new System.Drawing.Point(112, 68);
+            this._hr_.Name = "_hr_";
+            this._hr_.Size = new System.Drawing.Size(238, 13);
+            this._hr_.TabIndex = 9;
+            this._hr_.Text = "-----------------------------------------------------------------------------";
+            // 
+            // __hr
+            // 
+            this.@__hr.AutoSize = true;
+            this.@__hr.Location = new System.Drawing.Point(110, 101);
+            this.@__hr.Name = "__hr";
+            this.@__hr.Size = new System.Drawing.Size(238, 13);
+            this.@__hr.TabIndex = 10;
+            this.@__hr.Text = "-----------------------------------------------------------------------------";
+            // 
+            // errore
+            // 
+            this.errore.AutoSize = true;
+            this.errore.Location = new System.Drawing.Point(172, 85);
+            this.errore.Name = "errore";
+            this.errore.Size = new System.Drawing.Size(0, 13);
+            this.errore.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Goudy Stout", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(405, 525);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 39);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Marelio Bejko";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 417);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.errore);
+            this.Controls.Add(this.@__hr);
+            this.Controls.Add(this._hr_);
+            this.Controls.Add(this.lstbox_messagio_server);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.txt_mesaggio);
+            this.Controls.Add(this.Messagio);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.txt_serverport);
+            this.Controls.Add(this.txt_severip);
+            this.Controls.Add(this.Serverport);
+            this.Controls.Add(this.Serverip);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ClientChat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Serverip;
+        private System.Windows.Forms.Label Serverport;
+        private System.Windows.Forms.TextBox txt_severip;
+        private System.Windows.Forms.TextBox txt_serverport;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Label Messagio;
+        private System.Windows.Forms.TextBox txt_mesaggio;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.ListBox lstbox_messagio_server;
+        private System.Windows.Forms.Label _hr_;
+        private System.Windows.Forms.Label __hr;
+        private System.Windows.Forms.Label errore;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FormChatBot_Milosevic/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FormChatBot_Milosevic
+{
+    public partial class Form1 : Form
+    {
+        Socket client;
+        IPAddress ipAddr = null;
+        string strIPAddress = "";
+        string strPort = "";
+        int nPort = 0;
+        string sendString = "";
+        string recvString = "";
+        byte[] sendBuff = new byte[256];
+        byte[] recvBuff = new byte[256];
+        int recvBytes = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            Size = new Size(501, 156);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            btn_Send.Visible = false;
+            txt_mesaggio.Visible = false;
+            Messagio.Visible = false;
+            lstbox_messagio_server.Visible = false;
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+
+            strIPAddress = txt_severip.Text;
+            strPort = txt_serverport.Text;
+            if (!IPAddress.TryParse(strIPAddress.Trim(), out ipAddr))
+            {
+                errore.Text = "L'IP inserito non è valido!!";//se fallisco do il messaggio e chiudo il programma
+                return;
+            }
+            // Provo a copiare la porta in forma di stringa nella varibile intera
+            if (!int.TryParse(strPort, out nPort))
+            {
+                errore.Text = "La porta inserita non è valida!!"; //se fallisco do il messaggio e chiudo il programma
+                return;
+            }
+            // Controllo che la porta sia compresa fra 0 e 65535
+            if (nPort <= 0 || nPort >= 65535)
+            {
+                errore.Text = "La porta non è valida!!"; //se fallisco do il messaggio e chiudo il programma
+                return;
+            }
+            errore.Text = "Endpoint: " + ipAddr.ToString() + " " + nPort;
+
+            // Connessione al server
+            try
+            {
+                client.Connect(ipAddr, nPort);
+                errore.Text = "Connessione Riuscita!!";
+
+                txt_severip.Enabled = false;
+                txt_serverport.Enabled = false;
+                btn_connect.Enabled = false;
+
+                Size = new Size(501, 399);
+                btn_Send.Visible = true;
+                txt_mesaggio.Visible = true;
+                Messagio.Visible = true;
+                lstbox_messagio_server.Visible = true;
+                lstbox_messagio_server.Items.Add("Ora puoi parlare con il server!!");
+
+            }
+            catch (Exception ex)
+            {
+                errore.Text = ex.Message;
+            }
+
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            sendBuff = Encoding.ASCII.GetBytes(txt_mesaggio.Text);
+            if (txt_mesaggio.Text.ToUpper().Trim() != "QUIT")
+            {
+                client.Send(sendBuff);
+                // mi metto in ascolto del messaggio del server
+                recvBytes = client.Receive(recvBuff);
+                recvString = Encoding.ASCII.GetString(recvBuff);
+
+
+                lstbox_messagio_server.Items.Add("Client: " + txt_mesaggio.Text);
+                string[] subs = recvString.Split(';');
+
+                lstbox_messagio_server.Items.Add("Server: ");
+                foreach (string element in subs)
+                {
+                    lstbox_messagio_server.Items.Add(element);
+                }
+
+                //lo scrivo a video
+
+
+                //Pulisco le variabili
+                Array.Clear(recvBuff, 0, recvBuff.Length);
+                Array.Clear(sendBuff, 0, sendBuff.Length);
+                recvString = "";
+                sendString = "";
+                recvBytes = 0;
+                txt_mesaggio.Text = "";
+
+            }
+
+            if (txt_mesaggio.Text.ToUpper().Trim() == "QUIT")
+            {
+                btn_Send.Visible = false;
+                txt_mesaggio.Visible = false;
+                Messagio.Visible = false;
+                lstbox_messagio_server.Visible = false;
+                client.Send(sendBuff);
+                client.Close();
+                client.Dispose();
+                errore.Text = "Ti sei disconneso :)";
+                Size = new Size(501, 156);
+
+                txt_severip.Enabled = true;
+                txt_serverport.Enabled = true;
+                btn_connect.Enabled = true;
+
+            }
+        }
+    }
+}
